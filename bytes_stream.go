e04fc6985b69f834ef1ae1a6a925f844030e3117
shiwei <shiwei0124@gmail.com>
2015-09-11 18:02:17 +0800

    [CHG] 增加一个二进制读取的类

---
+++ bytes_stream.go
@@ -0,0 +1,60 @@
+// bytes_data.go
+package gobase
+
+import (
+	"fmt"
+	"encoding/binary"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+type BytesStream struct {
+	data []byte
+	pos int
+}
+
+func NewBytesStream(data []byte) * BytesStream {
+	bytesStream := &BytesStream {
+		data : data,
+		pos : 0,
+	}
+	return bytesStream
+}
+ 
+func (s * BytesStream) ReadByte() byte {
+	res := s.data[s.pos]
+	s.pos += 1
+	return res
+}
+
+func (s * BytesStream) ReadUint16() uint16 {
+	res := binary.BigEndian.Uint16(s.data[s.pos:s.pos+2])
+	s.pos += 2
+	return res
+}
+
+func (s * BytesStream) ReadUint32() uint32 {
+	res := binary.BigEndian.Uint32(s.data[s.pos:s.pos+4])
+	s.pos += 4
+	return res
+}
+
+func (s * BytesStream) ReadUint64() uint64 {
+	res := binary.BigEndian.Uint64(s.data[s.pos:s.pos+8])
+	s.pos += 8
+	return res
+}
+
+func (s * BytesStream) ReadString(length int) string {
+	res := string(s.data[s.pos:s.pos+length])
+	s.pos += length
+	return res
+}
+
+func (s * BytesStream) ReadBytes(length int) []byte {
+	res := s.data[s.pos:s.pos+length]	
+	s.pos += length
+	return res
+}
