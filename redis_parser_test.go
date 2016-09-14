468d7f5d0a3ae91dcbb2ecec59aafc1dd9870bc8
shiwei <shiwei0124@gmail.com>
2016-09-14 14:24:25 +0800

    [CHG] 更新测试代码

--- redis_parser_test.go
+++ redis_parser_test.go
@@ -10,11 +10,20 @@ func Test_ParseRedis(b *testing.T) {
 	//data = "*3\r\n:1\r\n:-2\r\n:3\r\n"
 	data = "*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Foo\r\n-Bar\r\n"
 	//data = "*2\r\n+Foo\r\n-Bar\r\n"
-	cmd := NewRedisCmd([]byte(data))
-	if respData, err := cmd.ParseRequest(); err != nil {
-		fmt.Println(err)
-	} else {
-		printRESP(respData)
-		//fmt.Println(type(a))
+	fmt.Println("ddddd")
+	n := 1000000
+	for i := 0; i < n; i++ {
+		cmd := NewRedisCmd([]byte(data))
+		if _, err := cmd.ParseRequest(); err != nil {
+			fmt.Println(err)
+		} else {
+			//printRESP(respData)
+			//fmt.Println(type(a))
+		}
 	}
+	fmt.Println("aaa")
+}
+
+func Test_ParseLargeRESP(b *testing.T) {
+
 }
